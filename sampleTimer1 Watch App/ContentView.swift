6326495df20d1311a12f66fcf32e83a1ddbf7abd
morisum <moris@r.sannet.ne.jp>
2023-10-14 05:55:39 +0900

    Initial Commit

---
+++ sampleTimer1 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sampleTimer1 Watch App
+//
+//  Created by MsMacM on 2023/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sampleTimer1 Watch App/sampleTimer1App.swift
@@ -0,0 +1,17 @@
+//
+//  sampleTimer1App.swift
+//  sampleTimer1 Watch App
+//
+//  Created by MsMacM on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct sampleTimer1_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
