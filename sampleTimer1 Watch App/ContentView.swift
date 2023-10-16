de2f4ee2012fab9bcc0bd023ae2cb2445ef54941
morisum <moris@r.sannet.ne.jp>
2023-10-17 08:25:51 +0900

    書いてあるところまで完成 キャンセルボタンが効かない

--- sampleTimer1 Watch App/ContentView.swift
+++ sampleTimer1 Watch App/ContentView.swift
@@ -3,7 +3,7 @@
 //  sampleTimer1 Watch App
 //
 //  Created by MsMacM on 2023/10/14.
-//
+//  https://zenn.dev/ryo_kawamata/articles/timer-app-with-swift-ui
 
 import SwiftUI
 
@@ -12,33 +12,93 @@ struct ContentView: View {
     @State var timerScreenShow: Bool = false
     
     var body: some View {
-        VStack {
-            Text("Timer \(self.timeVal) 秒").font(.body)
-            Picker(selection: self.$timeVal, label: Text("000").font(.subheadline)) {
-                Text("1").tag(1).font(.title2)
-                Text("5").tag(5).font(.title2)
-                Text("10").tag(10).font(.title2)
-                Text("30").tag(30).font(.title2)
-                Text("60").tag(60).font(.title2)
-            }
-            NavigationStack{
-                NavigationLink{
-                    TimerView(timerScreenShow: $timerScreenShow, timeVal: timeVal)
-                } label: {
-                    Label("timer", systemImage: "figure.walk")
-                        .font(.title3)
+        NavigationStack{
+            VStack {
+                Text("Timer \(timeVal) 秒").font(.body)
+                Picker(selection: self.$timeVal, label: Text("000").font(.subheadline)) {
+                    Text("1").tag(1).font(.title2)
+                    Text("5").tag(5).font(.title2)
+                    Text("10").tag(10).font(.title2)
+                    Text("30").tag(30).font(.title2)
+                    Text("60").tag(60).font(.title2)
                 }
             }
+            
+            NavigationLink{
+                TimerView(timerScreenShow: $timerScreenShow, timeVal: timeVal, initialTime: timeVal)
+            } label: {
+                Label("timer", systemImage: "figure.walk")
+                    .font(.headline)
+//                    .frame(width: 100, height: 100)
+//                    .foregroundColor(.red)
+            }
+//            .frame(width: 200, height:10)
+//            .background(.blue)
         }
-        .padding()
     }
 }
+
 struct TimerView: View {
     @Binding var timerScreenShow: Bool
     @State var timeVal: Int
+    let initialTime: Int
     
     var body: some View {
-        Text("\(self.timeVal)")
+//        Text("\(self.timeVal)")
+        if timeVal > -1 {
+            VStack {
+                ZStack {
+                    Text("\(timeVal)").font(.system(size: 40))
+                        .onAppear() {
+                            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {_ in 
+                                if timeVal > -1 {
+                                    timeVal -= 1
+                                }
+                            }
+                    }
+                    ProgressBar(progress: timeVal, initial: initialTime)
+                        .frame(width: 90.0, height: 90.0)
+                }
+                
+                Button(action: {
+                    timerScreenShow = false
+                }, label: {
+                    Text("Cancel")
+                        .foregroundColor(.red)
+                })
+                .padding(.top)
+            }
+        } else {
+            Button(action: {
+                timerScreenShow = false
+            }, label: {
+                Text("Done!")
+                    .font(.title)
+                    .foregroundColor(.green)
+            }).onAppear() {
+                WKInterfaceDevice.current().play(.notification)
+            }
+        }
+    }
+}
+
+struct ProgressBar: View {
+    let progress: Int
+    let initial: Int
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 15.0)
+                .opacity(0.3)
+                .foregroundColor(.red)
+            Circle()
+                .trim(from: 0.0, to: CGFloat(min(Float(progress) / Float(initial), 1.0)))
+                .stroke(style: StrokeStyle(lineWidth: 15.0, lineCap: .round, lineJoin: .round))
+                .foregroundColor(.red)
+                .rotationEffect(Angle(degrees: 270.0))
+                .animation(.easeIn, value: 1.0)//?????
+        }
     }
 }
 
